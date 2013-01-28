c01df64b338a6a96119a40175e790931447396a1
Andrew Mauboussin <ammaub@college.harvard.edu>
2013-01-28 12:21:59 -0500

    added new option to set the number of tickmarks/labels that should be drawn

--- number_line.py
+++ number_line.py
@@ -1,13 +1,20 @@
 import string
 import pylab as plt
 
+#Number line plotting with matplotlib.
+
+#TODO 
+#Allow "barbell" plots like 2<x<4 or x<2 *intersection* x>4
+
 class number_line(object):
-	def __init__(self, radius=10):
+	def __init__(self, radius=10, tickmarks = 20):
 
 		self.radius = radius
 		self.span = radius * 1.1 #edges of the graph. leaves room for arrows
 		self.ceil = radius + 1 #if a plot goes to this point it means it is going to infinity
 		self.floor = -radius - 1 #if a plot goes to this point it means it is going to negative infinity
+		
+		self.num_tickmarks = tickmarks
 		
 		#constants
 		self.black_linewidth = 3
@@ -25,21 +32,25 @@ class number_line(object):
 		self.ax.axes.get_yaxis().set_visible(False) # turn off y axis
 		self.ax.get_xaxis().tick_bottom() #disable ticks at top of plot
 		self.ax.axes.get_xaxis().set_ticks([]) #sets ticks on bottom to blank
-		plt.axis([-self.span, self.span, -1, 1]) # sets dimensions of axes to xmin, xmax, ymin, ymax
+		plt.axis([-self.span, self.span, -1, 1]) # sets dimensions of axes
 	
+	#draws the number line
 	def drawLine(self):
-		#plot the main line  (slightly crooked so svg will render)
+		
+		#plot the main line  (slightly crooked so svg will render in webkit)
 		plt.plot([-self.radius,self.radius],[0,0.001],'k',linewidth=self.black_linewidth,zorder=1) 
 
-
-		#tick marks and labels
-		for i in range(-self.radius,self.radius+1):
+		#find the interval for tickmarks/labels (divide by two to account for negative numbers)
+		tickmark_interval = self.radius/ (self.num_tickmarks/2)
+		
+		#draw in appropriate places tick marks and labels
+		for i in [num for num in range(-self.radius,self.radius+1) if not num % tickmark_interval] :
 			length = .03
 			tick_y = -.1
 			if i==0: 
 				length=.05
 				tick_y = -.13
-			 #plot the tick marks (slightly crooked so svg will render)
+			 #plot the tick marks (slightly crooked so svg will render in webkit)
 			plt.plot([i,i-.001],[-length,length],'k',linewidth=2, zorder=1) 
 			
 			#write the labels
@@ -60,10 +71,15 @@ class number_line(object):
 	#graph a series of inequalities
 	def graph(self,eqs):
 		
+		#make equations into a list if it is not already
+		if isinstance(eqs, str):
+			eqs = [eqs]
+		
+		#get coordinates to graph
 		toGraph = []
 		for eq in eqs:
 			toGraph = self.solve(eq,toGraph)
-
+	
 		for piece in toGraph:
 			
 			#if this piece of the graph is a point
@@ -73,7 +89,7 @@ class number_line(object):
 			#if this piece of the graph is a line
 			elif len(piece)==2: 
 			
-				#if line going to negative infinity with arrow
+				#if line going to negative infinity with arrow draw 
 				if piece[0]==self.floor: 
 				
 					plt.plot([piece[1]],[0],'or',markerfacecolor='white',markeredgecolor='r',markersize = self.circle_size, markeredgewidth = self.circle_edge_width, zorder=10)
@@ -87,10 +103,9 @@ class number_line(object):
 					plt.plot([piece[0],self.radius],[0,0],'-r',linewidth=self.red_linewidth,zorder=5)
 					self.arrow((self.span,0),'r',facingRight = True)
 				
-	#helper method for graph turns the equations into coordinates
+	#Turns an equation into coordinates to plot
 	def solve(self,eq, toGraph):
 	    
-		
 	    if '>' in eq:
 	        toGraph.append([self.getAfter(eq,'>') , self.ceil])
 	    elif '<' in eq:
@@ -100,11 +115,28 @@ class number_line(object):
 	        toGraph.append([self.getAfter(eq,'=')])
 	    return toGraph
 	
-	#helper method for solve helps parse the equations
-	def getAfter(self,s, char ):
-		if'>=' in s: char = '>='
-		if '<=' in s: char = '<='
-		return float(string.split(s,char)[-1])
+	#gets float of text in a string after the <, >, <=, or >= sign to help solve equation
+	def getAfter(self,s, splitchar ):
+		if'>=' in s: 
+			splitchar = '>='
+		if '<=' in s: 
+			splitchar = '<='
+		
+		#check if number is valid
+		try:
+			number = float(string.split(s, splitchar)[-1])
+		except ValueError:
+			raise Exception("Error parsing equations. Make sure they are of the format x>1 where x is any variable and 1 is any number")
+			
+		#check if within bounds
+		if number < -self.radius or number > self.radius:
+			raise Exception ("Some points are outside the number line's bounds. Try increasing the radius of the number line or choosing values inside the current radius.")
+		return number
+
+
+	#save figure to specified location
+	def save(self, filepath, format='png', transparent = True):
+		plt.savefig(filepath, format= format, bbox_inches='tight',transparent = transparent)
 
 	#clears anything that is graphed
 	def clear(self):
--- number_line_example.py
+++ number_line_example.py
@@ -1,17 +1,20 @@
 from number_line import number_line
-import pylab as plt
-
-numline = number_line(radius = 10)
 
 
+numline = number_line()
 examples = ['x>=7','x=4','x<2']
 
 #plot each equation one by one
-for s in examples:
-	numline.graph([s])
-	plt.savefig('/Users/Andrew/Documents/Matplotlib1D-plot/%s.png' % (s), format='png',bbox_inches='tight',transparent = True)
+for e in examples:
+	numline.graph(e)
+	numline.save('%s.png' % (e))
 	numline.clear()
 	
 #plot all equations on one graph
 numline.graph(examples)
-plt.savefig('/Users/Andrew/Documents/Matplotlib1D-plot/all.png', format='png',bbox_inches='tight',transparent = True)
+numline.save('all.png')
+
+#using different radius and number of tickmarks
+larger_numline = number_line(radius = 100, tickmarks = 4)
+larger_numline.graph('x>25')
+larger_numline.save('x>25.png')
